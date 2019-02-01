3080f9c49ce692d25533ad8e07a0e5dcd3949c64
anamercedes9 <anamercedes9@hotmail.com>
2019-02-01 17:30:37 -0400

    proyectos

---
+++ boletines/javascript/main.js
@@ -0,0 +1,28 @@
+let links = document.querySelectorAll(".close");
+
+// Recorrerlos
+
+links.forEach(function(link){
+
+    // Agregar un evento click a cada uno de ellos - case sensitive
+    link.addEventListener("click",function(ev){
+      ev.preventDefault();
+      let content = document.querySelector('.content');
+
+      // Quitarle las clases de animacion que ya tiene
+      content.classList.remove("fadeInDown");
+      content.classList.remove("animated");
+
+      // Agregar clases para animar su salida  fadeOutUp
+      content.classList.add("fadeOutUp");
+      content.classList.add("animated");
+
+      setTimeout(function(){
+        location.href = "/boletines";
+      },600);
+
+
+      return false;
+
+    });
+});
